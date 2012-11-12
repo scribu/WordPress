b3e79ae3d6ac3b069db3f5f984193b5ccaea299d
Nikolay Bachiyski <nb@nikolay.bg>
2012-11-12 19:08:44 +0200

    Add WP_Map_Iterator
    Accepts a callback and maps all the values using it.

--- wp-includes/iterators.php
+++ wp-includes/iterators.php
@@ -1,4 +1,15 @@
 <?php
+class WP_Map_Iterator extends IteratorIterator {
+	function __construct( $iterator, $callback ) {
+		$this->callback = $callback;
+		parent::__construct( $iterator );
+	}
+
+	function current() {
+		$original_current = parent::current();
+		return call_user_func( $this->callback, $original_current );
+	}
+}
 
 class WP_Post_IDs_Iterator implements Iterator {
 	private $limit = 100;

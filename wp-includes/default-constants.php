301434e15743be401d9259fb5113a855e05d5369
Nikolay Bachiyski <nb@nikolay.bg>
2013-01-26 15:37:58 +0200

    Remove duplicate YEAR_IN_SECONDS definition

--- wp-includes/default-constants.php
+++ wp-includes/default-constants.php
@@ -80,7 +80,6 @@ function wp_initial_constants() {
 	define( 'DAY_IN_SECONDS',    24 * HOUR_IN_SECONDS   );
 	define( 'WEEK_IN_SECONDS',    7 * DAY_IN_SECONDS    );
 	define( 'YEAR_IN_SECONDS',  365 * DAY_IN_SECONDS    );
- 	define( 'YEAR_IN_SECONDS',  365 * DAY_IN_SECONDS    );
 
 	// Constants for expressing human-readable data sizes
 	// in their respective number of bytes.
